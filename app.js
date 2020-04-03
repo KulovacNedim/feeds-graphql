8e79304c300287189fb644b087cbbd249f21742c
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-03 12:18:30 +0200

    createUser mutator and resolver created

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ app.use(
     '/graphql',
     graphqlHttp({
         schema: graphqlSchema,
-        rootValue: graphqlResolver
+        rootValue: graphqlResolver,
+        graphiql: true
     })
 );
 
