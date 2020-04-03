1b4869e35af8b75656f81e58231a8cadda10e755
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-03 18:19:33 +0200

    pagination implemented. image upload implemented

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const fs = require('fs');
 const multer = require('multer');
 const graphqlHttp = require('express-graphql');
 
@@ -48,6 +49,24 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('Not authenticated!');
+    }
+    if (!req.file) {
+        return res.status(200).json({
+            message: 'No file provided!'
+        });
+    }
+    if(req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res.status(201).json({
+        message: 'File stored!',
+        filePath: req.file.path
+    });
+});
+
 app.use(
     '/graphql',
     graphqlHttp({
@@ -85,3 +104,7 @@ mongoose.connect(
     })
     .catch(err => console.log(err));
 
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+};
